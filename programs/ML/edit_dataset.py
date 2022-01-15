03b7637ca30cd8fddd852090670180d7c791938b
kathmandu777 <mahhakatomanato@gmail.com>
2022-01-15 18:17:07 +0900

    update edit dataset

--- programs/ML/edit_dataset.py
+++ programs/ML/edit_dataset.py
@@ -6,20 +6,20 @@ import uuid
 
 def make_valid_dataset():
     for num in range(1, 76):
-        files = glob.glob(f'./train-dataset/{num}/*.jpg')
+        files = glob.glob(f'.//{num}/*.jpg')
         os.makedirs(f"./valid-dataset/{num}", exist_ok=True)
         shutil.move(files[0], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
         shutil.move(files[1], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
         shutil.move(files[2], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
 
 
-def count_dataset():
+def count_dataset(folder='train-dataset'):
     sum = 0
     for num in range(1, 76):
-        files = glob.glob(f'./unverified-dataset/{num}/*.jpg')
+        files = glob.glob(f'./{folder}/{num}/*.jpg')
         sum += len(files)
         print(f"{num}: {len(files)}")
-    files = glob.glob(f'./unverified-dataset/x/*.jpg')
+    files = glob.glob(f'./{folder}/x/*.jpg')
     print(f"x: {len(files)}")
     sum += len(files)
     print(f"sum: {sum}")
