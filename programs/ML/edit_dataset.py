17093d07e8cc3fb5641fc406965a8e58af737068
Manato Kato <57817950+kathmandu777@users.noreply.github.com>
2022-01-17 23:57:53 +0900

    Revert "Issue/12 ml update accuracy and integrate to scanner (#13)"
    This reverts commit 01ed472703285578ab1f9cbeb79cd3a754cefe69.

--- programs/ML/edit_dataset.py
+++ programs/ML/edit_dataset.py
@@ -3,28 +3,9 @@ import glob
 import shutil
 import uuid
 
-
-def make_valid_dataset():
-    for num in range(1, 76):
-        files = glob.glob(f'./train-dataset/{num}/*.jpg')
-        os.makedirs(f"./valid-dataset/{num}", exist_ok=True)
-        shutil.move(files[0], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
-        shutil.move(files[1], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
-        # shutil.move(files[2], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
-
-
-def count_dataset(folder='valid-dataset'):
-    sum = 0
-    for num in range(1, 76):
-        files = glob.glob(f'./{folder}/{num}/*.jpg')
-        sum += len(files)
-        print(f"{num}: {len(files)}")
-    files = glob.glob(f'./{folder}/x/*.jpg')
-    print(f"x: {len(files)}")
-    sum += len(files)
-    print(f"sum: {sum}")
-
-
-if __name__ == "__main__":
-    # make_valid_dataset()
-    count_dataset()
+for num in range(1, 76):
+    files = glob.glob(f'./train-dataset/{num}/*.jpg')
+    os.makedirs(f"./valid-dataset/{num}", exist_ok=True)
+    shutil.move(files[0], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
+    shutil.move(files[1], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
+    shutil.move(files[2], f'./valid-dataset/{num}/{uuid.uuid4()}.jpg')
